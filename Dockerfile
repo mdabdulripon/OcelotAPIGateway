a56772a203be0b0fb51d43d84568c20e4c00cf1a
Abdul Awal Ripon <aripon@Abduls-Mac-mini.local>
2024-11-24 09:25:26 -0600

    create ocelet api gateway and docker image

---
+++ Dockerfile
@@ -0,0 +1,32 @@
+﻿# Use .NET SDK image for building (ARM64)
+FROM mcr.microsoft.com/dotnet/sdk:8.0 AS build
+ARG BUILD_CONFIGURATION=Release
+WORKDIR /src
+
+# Copy csproj files individually to improve caching
+COPY ["OcelotAPIGateway/OcelotAPIGateway.csproj", "./"]
+RUN dotnet restore "OcelotAPIGateway.csproj"
+
+# Copy the entire project for building
+COPY . .
+
+# Set the working directory to Merchant.API and build the project
+WORKDIR /src/
+RUN dotnet build "OcelotAPIGateway/OcelotAPIGateway.csproj" -c $BUILD_CONFIGURATION -o /app/build
+
+# Publish project
+FROM build AS publish
+RUN dotnet publish "OcelotAPIGateway/OcelotAPIGateway.csproj" -c $BUILD_CONFIGURATION -o /app/publish /p:UseAppHost=false
+
+# Use ASP.NET runtime image for deployment (ARM64)
+FROM mcr.microsoft.com/dotnet/aspnet:8.0 AS final
+WORKDIR /app
+
+# Copy published output
+COPY --from=publish /app/publish .
+
+# Expose port (optional, adjust if necessary)
+EXPOSE 8080
+
+# Run the application
+ENTRYPOINT ["dotnet", "OcelotAPIGateway.dll"]
--- Program.cs
+++ Program.cs
@@ -1,44 +1,31 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+IConfiguration configuration = new ConfigurationBuilder()
+    .AddJsonFile($"ocelot.{builder.Environment.EnvironmentName}.json", true, true)
+    .Build();
+
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(builder =>
+    {
+        builder.WithOrigins("http://localhost:4100", "http://localhost:4200")
+            .WithHeaders("Content-Type", "Authorization")
+            .WithMethods("GET", "POST", "PUT", "DELETE");
+    });
+});
+
+builder.Services.AddOcelot(configuration);
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseCors();
 
-app.UseHttpsRedirection();
+await app.UseOcelot();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-    {
-        var forecast = Enumerable.Range(1, 5).Select(index =>
-                new WeatherForecast
-                (
-                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                    Random.Shared.Next(-20, 55),
-                    summaries[Random.Shared.Next(summaries.Length)]
-                ))
-            .ToArray();
-        return forecast;
-    })
-    .WithName("GetWeatherForecast")
-    .WithOpenApi();
+app.MapGet("/", () => "healthy");
 
 app.Run();
 
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
